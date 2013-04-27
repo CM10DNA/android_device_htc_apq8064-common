190642406e48d0c69b6f8a0b04cd8a996664a2d5
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-27 17:59:03 +0200

    apq8064: initial import from msm8960-common

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,90 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+BOARD_VENDOR := htc
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DNEW_ION_API=1
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Camera
+TARGET_PROVIDES_CAMERA_HAL := false
+USE_CAMERA_STUB := true
+
+# QCOM GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+TARGET_QCOM_DISPLAY_VARIANT := caf
+BOARD_EGL_CFG := device/htc/apq8064-common/configs/egl.cfg
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Power
+TARGET_PROVIDES_POWERHAL := true
+
+# RIL
+BOARD_RIL_CLASS := "../../../device/htc/apq8064-common/libril/"
+
+# Time
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
