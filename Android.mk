634ea84329175be5bd91f7a329196ada4b0a47ae
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-04-28 06:22:27 +0200

    lock it against other devices

--- Android.mk
+++ Android.mk
@@ -16,8 +16,9 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),htc)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(filter m7 m7wls,$(TARGET_DEVICE)),)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 endif
-endif
+
