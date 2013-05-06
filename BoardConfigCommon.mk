b57481a6c9d7449e2a2343415b94b12afb0c8280
Richard Ross <toastcfh@gmail.com>
2013-05-05 21:27:38 -0400

    BoardConfigCommon: remove common kernel directory define.
    Change-Id: I9caa5b60f7122263d1f0fe03e7a581b0f4a6c17d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,9 +29,6 @@ BOARD_VENDOR := htc
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/htc/msm8960
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
